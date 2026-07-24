5fdabda8e2b7717435c5631c8161c455798c5dac
Atmadja <sudira.kevin@hotmail.fr>
2026-07-24 16:40:01 +0200

    corrected imports

--- apps/api/src/services/cron/cron.module.ts
+++ apps/api/src/services/cron/cron.module.ts
@@ -2,18 +2,12 @@ import { ActivitiesModule } from '@ghostfolio/api/app/activities/activities.modu
 import { ImportModule } from '@ghostfolio/api/app/import/import.module';
 import { PortfolioModule } from '@ghostfolio/api/app/portfolio/portfolio.module';
 import { UserModule } from '@ghostfolio/api/app/user/user.module';
-import { UserService } from '@ghostfolio/api/app/user/user.service';
 import { ConfigurationModule } from '@ghostfolio/api/services/configuration/configuration.module';
-import { ConfigurationService } from '@ghostfolio/api/services/configuration/configuration.service';
 import { PrismaModule } from '@ghostfolio/api/services/prisma/prisma.module';
 import { PropertyModule } from '@ghostfolio/api/services/property/property.module';
-import { PropertyService } from '@ghostfolio/api/services/property/property.service';
 import { DataGatheringQueueModule } from '@ghostfolio/api/services/queues/data-gathering/data-gathering.module';
-import { DataGatheringService } from '@ghostfolio/api/services/queues/data-gathering/data-gathering.service';
 import { StatisticsGatheringQueueModule } from '@ghostfolio/api/services/queues/statistics-gathering/statistics-gathering.module';
-import { StatisticsGatheringService } from '@ghostfolio/api/services/queues/statistics-gathering/statistics-gathering.service';
 import { TwitterBotModule } from '@ghostfolio/api/services/twitter-bot/twitter-bot.module';
-import { TwitterBotService } from '@ghostfolio/api/services/twitter-bot/twitter-bot.service';
 
 import { Module } from '@nestjs/common';
 
